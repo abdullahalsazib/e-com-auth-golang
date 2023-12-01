cb1b58d6881f3f8f6c6743c9a3693cd9b9afb41e
Neeraj Avinash <neerajbg@gmail.com>
2023-12-01 18:46:36 +0530

    Code cleanup

--- model/user.go
+++ model/user.go
@@ -5,6 +5,6 @@ import "time"
 type User struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Email     string    `json:"email" gorm:"column:email;unique"`
-	Password  string    `json:"_" gorm:"column:password;"`
+	Password  string    `json:"-" gorm:"column:password;"`
 	CreatedAt time.Time `json:"created_at" gorm:"column:created_at;autoCreateTime"`
 }
